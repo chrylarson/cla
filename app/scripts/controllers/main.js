2fa90d2b950f27a2635f5b1df3d0497c47dc7156
Chris Larson <chris@server.(none)>
2014-02-17 08:33:33 -0600

    two-way binding working between force diagram directives and controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,26 @@ angular.module('claApp')
       'Karma'
     ];
 
-$scope.nodes =
-{
-"nodes":
-[    {"name":"Front Panel Object to JSON.vi","group":2},
-     {"name":"Array to JSON Object.vi","group":2},
-     {"name":"Scale to JSON.vi","group":2},
-     {"name":"Caption to JSON.vi","group":2},
-     {"name":"Label to JSON.vi","group":2}],
-"links":
-[    {"source":0,"target":1,"value":4},
-     {"source":0,"target":2,"value":4},
-     {"source":0,"target":3,"value":4},
-     {"source":0,"target":4,"value":4}]
-}
-
+	$scope.data =
+	{nodes:
+		{
+		"nodes":
+		[    {"name":"Front Panel Object to JSON.vi","group":2},
+		     {"name":"Array to JSON Object.vi","group":2},
+		     {"name":"Scale to JSON.vi","group":2},
+		     {"name":"Caption to JSON.vi","group":2},
+		     {"name":"Label to JSON.vi","group":2}],
+		"links":
+		[    {"source":0,"target":1,"value":4},
+		     {"source":0,"target":2,"value":4},
+		     {"source":0,"target":3,"value":4},
+		     {"source":0,"target":4,"value":4}]
+		}
+	}
+ 		$scope.$watch('data.nodes', function(newValue, oldValue) {       
+ 			console.log("What"); 
+            if (newValue !== oldValue) {
+                console.log('This is coming from the controller to the directive now = ', newValue);
+            }
+        },true);  
   });
--- app/scripts/directives/forceDiagram.js
+++ app/scripts/directives/forceDiagram.js
@@ -10,122 +10,125 @@ angular.module('claApp')
             },
       link: function postLink(scope, element, attrs) {
 
+		function name(d) { return d.name; }
+		function group(d) { return d.group; }
+		var color = d3.scale.category10();
+		function colorByGroup(d) { return color(group(d)); }
 
-function name(d) { return d.name; }
-function group(d) { return d.group; }
-var data = scope.nodes;
-var color = d3.scale.category10();
-function colorByGroup(d) { return color(group(d)); }
+		var width = 500,
+		    height = 500;
 
-var width = 960,
-    height = 500;
+		var force = d3.layout.force()
+		    .charge(-2000)
+		    .friction(0.3)
+		    .linkDistance(50)
+		    .size([width, height])
+		    .on('tick', function() {
+			    node.attr('transform', function(d) { return 'translate('+d.x+','+d.y+')'; })
+			        .attr('clip-path', function(d) { return 'url(#clip-'+d.index+')'; });
 
-var svg = d3.select('#viz')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+			    link.attr('x1', function(d) { return d.source.x; })
+			        .attr('y1', function(d) { return d.source.y; })
+			        .attr('x2', function(d) { return d.target.x; })
+			        .attr('y2', function(d) { return d.target.y; });
 
-var voronoi = d3.geom.voronoi()
-    .x(function(d) { return d.x; })
-    .y(function(d) { return d.y; })
-    .clipExtent([[-10, -10], [width+10, height+10]]);
+			    var clip = svg.selectAll('.clip')
+			        .data( recenterVoronoi(node.data()), function(d) { return d.point.index; } );
 
-update();
+			    clip.enter().append('clipPath')
+			        .attr('id', function(d) { return 'clip-'+d.point.index; })
+			        .attr('class', 'clip');
+			    clip.exit().remove()
 
-function update() {
+			    clip.selectAll('path').remove();
+			    clip.append('path')
+			        .attr('d', function(d) { return 'M'+d.join(',')+'Z'; });
+			});
 
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
+		var svg = d3.select('#viz')
+		    .append('svg')
+		    .attr('width', width)
+		    .attr('height', height);
 
-var force = d3.layout.force()
-    .charge(-2000)
-    .friction(0.3)
-    .linkDistance(50)
-    .size([width, height]);
+		var link = svg.selectAll(".link"),
+		    node = svg.selectAll(".node");
 
-force.on('tick', function() {
-    node.attr('transform', function(d) { return 'translate('+d.x+','+d.y+')'; })
-        .attr('clip-path', function(d) { return 'url(#clip-'+d.index+')'; });
+		var voronoi = d3.geom.voronoi()
+		    .x(function(d) { return d.x; })
+		    .y(function(d) { return d.y; })
+		    .clipExtent([[-10, -10], [width+10, height+10]]);
 
-    link.attr('x1', function(d) { return d.source.x; })
-        .attr('y1', function(d) { return d.source.y; })
-        .attr('x2', function(d) { return d.target.x; })
-        .attr('y2', function(d) { return d.target.y; });
+		update();
 
-    var clip = svg.selectAll('.clip')
-        .data( recenterVoronoi(node.data()), function(d) { return d.point.index; } );
+		function update() {
+		    force
+		        .nodes( scope.nodes.nodes )
+		        .links( scope.nodes.links )
+		        .start();
 
-    clip.enter().append('clipPath')
-        .attr('id', function(d) { return 'clip-'+d.point.index; })
-        .attr('class', 'clip');
-    clip.exit().remove()
+		    scope.nodes.nodes.forEach(function(d, i) {
+		        d.id = i;
+		    });
 
-    clip.selectAll('path').remove();
-    clip.append('path')
-        .attr('d', function(d) { return 'M'+d.join(',')+'Z'; });
-});
+		    link = link.data( scope.nodes.links );
+		    link.exit().remove();
+		    link.enter().append('line')
+		        .attr('class', 'link')
+		        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
+		    node = node.data( scope.nodes.nodes );
+		    node.exit().remove();
+		    node.enter().append('g')
+		        .attr('title', name)
+		        .attr('class', 'node')
+		        .call( force.drag );
 
-    data.nodes.forEach(function(d, i) {
-        d.id = i;
-    });
+			node.append("image")
+			  .attr("xlink:href", "http://forums.ni.com/ni/attachments/ni/170/527271/1/LV2010.ico")
+			  .attr("x", -8)
+			  .attr("y", -8)
+			  .attr("width", 16)
+			  .attr("height", 16)
+			  .on("click", click);
+		}
 
-    link = svg.selectAll('.link')
-        .data( data.links )
-        .enter().append('line')
-        .attr('class', 'link')
-        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+		function recenterVoronoi(nodes) {
+		    var shapes = [];
+		    voronoi(nodes).forEach(function(d) {
+		        if ( !d.length ) return;
+		        var n = [];
+		        d.forEach(function(c){
+		            n.push([ c[0] - d.point.x, c[1] - d.point.y ]);
+		        });
+		        n.point = d.point;
+		        shapes.push(n);
+		    });
+		    return shapes;
+		}
 
-    node = svg.selectAll('.node')
-        .data( data.nodes )
-        .enter().append('g')
-        .attr('title', name)
-        .attr('class', 'node')
-        .call( force.drag );
+		// Toggle children on click.
+		function click(d) {
+			console.log("clicked");
+		  if (!d3.event.defaultPrevented) {	
+		  	scope.nodes.links.forEach(function (dlink, index) {
+		  		console.log(dlink.source.id + " : " + dlink.target.id + " : " + d.id);
+		            if (dlink.source.id === d.id || dlink.target.id === d.id) {
+		            	console.log("true");
+		                scope.nodes.links.splice(index, 1);
+		            }
+		        });
+		  	scope.nodes.nodes.splice(d.index, 1);
+		    update();
+		  }
+		}
 
-	node.append("image")
-	  .attr("xlink:href", "http://forums.ni.com/ni/attachments/ni/170/527271/1/LV2010.ico")
-	  .attr("x", -8)
-	  .attr("y", -8)
-	  .attr("width", 16)
-	  .attr("height", 16);
-	//  .on("click", click);
+		scope.$watch('nodes', function(newValue, oldValue) {  	     
+            if (newValue !== oldValue) {
+                console.log("Diagram Update"); 
+                update();
+            }
+        },true); 
 
-	// node.append("text")
-	//   .attr("dx", 12)
-	//   .attr("dy", ".35em")
-	//   .text(function(d) { return d.name });
-
-    force
-        .nodes( data.nodes )
-        .links( data.links )
-        .start();
-}
-
-function recenterVoronoi(nodes) {
-    var shapes = [];
-    voronoi(nodes).forEach(function(d) {
-        if ( !d.length ) return;
-        var n = [];
-        d.forEach(function(c){
-            n.push([ c[0] - d.point.x, c[1] - d.point.y ]);
-        });
-        n.point = d.point;
-        shapes.push(n);
-    });
-    return shapes;
-}
-
-// Toggle children on click.
-function click(d) {
-	console.log("clicked");
-	console.log(d);
-  if (!d3.event.defaultPrevented) {
-  	data.nodes.splice(d.index, 1);
-  	console.log(data);
-    update();
-  }
-}
       }
     };
   });
